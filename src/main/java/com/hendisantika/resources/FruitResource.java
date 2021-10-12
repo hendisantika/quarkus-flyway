25c8d9d249a56f41f7cf1116350c5d699f66438e
Hendi Santika <hendisantika@gmail.com>
2021-10-12 12:52:46 +0700

    #8 - Add list Fruits API in FruitResource

--- src/main/java/com/hendisantika/resources/FruitResource.java
+++ src/main/java/com/hendisantika/resources/FruitResource.java
@@ -1,9 +1,13 @@
 package com.hendisantika.resources;
 
+import com.hendisantika.entity.Fruit;
+
 import javax.ws.rs.Consumes;
+import javax.ws.rs.GET;
 import javax.ws.rs.Path;
 import javax.ws.rs.Produces;
 import javax.ws.rs.core.MediaType;
+import java.util.Collection;
 
 /**
  * Created by IntelliJ IDEA.
@@ -19,4 +23,9 @@ import javax.ws.rs.core.MediaType;
 @Produces(MediaType.APPLICATION_JSON)
 @Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
+    @GET
+    public Collection<Fruit> list() {
+        return Fruit.listAll();
+    }
+
 }
