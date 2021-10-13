f5008c6edf403fab87b5c523f3e9939b868c5226
Hendi Santika <hendisantika@gmail.com>
2021-10-13 23:23:11 +0700

    #23 - Bug Fix code

--- src/main/java/com/hendisantika/entity/Fruit.java
+++ src/main/java/com/hendisantika/entity/Fruit.java
@@ -1,6 +1,10 @@
 package com.hendisantika.entity;
 
-import io.quarkus.hibernate.orm.panache.PanacheEntityBase;
+
+import io.quarkus.hibernate.reactive.panache.Panache;
+import io.quarkus.hibernate.reactive.panache.PanacheEntityBase;
+import io.quarkus.panache.common.Sort;
+import io.smallrye.mutiny.Uni;
 
 import javax.persistence.Entity;
 import javax.persistence.GeneratedValue;
@@ -8,6 +12,9 @@ import javax.persistence.GenerationType;
 import javax.persistence.Id;
 import javax.persistence.SequenceGenerator;
 import javax.persistence.Table;
+import java.time.Duration;
+import java.util.Collections;
+import java.util.List;
 
 /**
  * Created by IntelliJ IDEA.
@@ -32,4 +39,50 @@ public class Fruit extends PanacheEntityBase {
 
     public String name;
     public String description;
+
+    public static Uni<Fruit> findByFruitId(Long id) {
+        return findById(id);
+    }
+
+    public static Uni<Fruit> updateFruit(Long id, Fruit fruit) {
+        return Panache
+                .withTransaction(() -> findByFruitId(id)
+                        .onItem().ifNotNull()
+                        .transform(entity -> {
+                            entity.name = fruit.name;
+                            entity.description = fruit.description;
+                            return entity;
+                        })
+                        .onFailure().recoverWithNull());
+    }
+
+    public static Uni<Fruit> addFruit(Fruit fruit) {
+        return Panache
+                .withTransaction(fruit::persist)
+                .replaceWith(fruit)
+                .ifNoItem()
+                .after(Duration.ofMillis(10000))
+                .fail()
+                .onFailure()
+                .transform(t -> new IllegalStateException(t));
+    }
+
+    public static Uni<List<Fruit>> getAllFruits() {
+        return Fruit
+                .listAll(Sort.by("createdAt"))
+                .ifNoItem()
+                .after(Duration.ofMillis(10000))
+                .fail()
+                .onFailure()
+                .recoverWithUni(Uni.createFrom().<List<PanacheEntityBase>>item(Collections.EMPTY_LIST));
+
+    }
+
+    public static Uni<Boolean> deleteFruit(Long id) {
+        return Panache.withTransaction(() -> deleteById(id));
+    }
+
+    public String toString() {
+        return this.getClass().getSimpleName() + "<" + this.id + ">";
+    }
 }
--- src/main/java/com/hendisantika/resources/FruitResource.java
+++ src/main/java/com/hendisantika/resources/FruitResource.java
@@ -1,19 +1,23 @@
 package com.hendisantika.resources;
 
 import com.hendisantika.entity.Fruit;
-import org.jboss.resteasy.annotations.jaxrs.PathParam;
+import io.smallrye.mutiny.Uni;
 
 import javax.transaction.Transactional;
 import javax.ws.rs.Consumes;
 import javax.ws.rs.DELETE;
 import javax.ws.rs.GET;
-import javax.ws.rs.NotFoundException;
 import javax.ws.rs.POST;
-import javax.ws.rs.PUT;
 import javax.ws.rs.Path;
+import javax.ws.rs.PathParam;
 import javax.ws.rs.Produces;
+import javax.ws.rs.WebApplicationException;
 import javax.ws.rs.core.MediaType;
-import java.util.Collection;
+import javax.ws.rs.core.Response;
+import java.net.URI;
+
+import static javax.ws.rs.core.Response.Status.NOT_FOUND;
+import static javax.ws.rs.core.Response.Status.OK;
 
 /**
  * Created by IntelliJ IDEA.
@@ -30,52 +34,80 @@ import java.util.Collection;
 @Consumes(MediaType.APPLICATION_JSON)
 public class FruitResource {
     @GET
-    public Collection<Fruit> list() {
-        return Fruit.listAll();
+    public Uni<Response> getFruitsList() {
+        return Fruit.getAllFruits()
+                .onItem().transform(products -> Response.ok(products))
+                .onItem().transform(Response.ResponseBuilder::build);
     }
 
     @GET
     @Path("/{id}")
-    public Fruit get(@PathParam Integer id) {
-        Fruit f = Fruit.findById(id);
-        if (f != null) {
-            return f;
-        } else {
-            throw new NotFoundException("Unknown fruit id : " + id);
-        }
+//    public Fruit get(@PathParam Integer id) {
+//        Fruit f = Fruit.findById(id);
+//        if (f != null) {
+//            return f;
+//        } else {
+//            throw new NotFoundException("Unknown fruit id : " + id);
+//        }
+//    }
+    public Uni<Response> getSingleFruit(@PathParam("id") Long id) {
+        return Fruit.findByFruitId(id)
+                .onItem().ifNotNull().transform(product -> Response.ok(product).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
     }
 
     @POST
     @Transactional
-    public Collection<Fruit> add(Fruit fruit) {
-        fruit.id = null; //ignore id
-        Fruit.persist(fruit);
-        return Fruit.listAll();
+//    public Collection<Fruit> add(Fruit fruit) {
+//        fruit.id = null; //ignore id
+//        Fruit.persist(fruit);
+//        return Fruit.listAll();
+//    }
+    @Produces(MediaType.APPLICATION_JSON)
+    @Consumes(MediaType.APPLICATION_JSON)
+    public Uni<Response> add(Fruit fruit) {
+        return Fruit.addFruit(fruit)
+                .onItem().transform(id -> URI.create("/v1/fruits/" + id.id))
+                .onItem().transform(uri -> Response.created(uri))
+                .onItem().transform(Response.ResponseBuilder::build);
     }
 
-    @PUT
+    @Path("{id}")
     @Transactional
-    public Fruit update(Fruit fruit) {
-        Fruit fruitUpdated = Fruit.findById(fruit.id);
-        if (fruitUpdated != null) {
-            fruitUpdated.name = fruit.name;
-            fruitUpdated.description = fruit.description;
-            Fruit.persist(fruitUpdated);
-            return fruitUpdated;
-        } else {
-            throw new NotFoundException("Unknown fruit id : " + fruit.id);
+//    public Fruit update(Fruit fruit) {
+//        Fruit fruitUpdated = Fruit.findById(fruit.id);
+//        if (fruitUpdated != null) {
+//            fruitUpdated.name = fruit.name;
+//            fruitUpdated.description = fruit.description;
+//            Fruit.persist(fruitUpdated);
+//            return fruitUpdated;
+//        } else {
+//            throw new NotFoundException("Unknown fruit id : " + fruit.id);
+//        }
+//    }
+    public Uni<Response> update(@PathParam("id") Long id, Fruit fruit) {
+        if (fruit == null || fruit.description == null) {
+            throw new WebApplicationException("Product description was not set on request.", 422);
         }
-
+        return Fruit.updateFruit(id, fruit)
+                .onItem().ifNotNull().transform(entity -> Response.ok(entity).build())
+                .onItem().ifNull().continueWith(Response.ok().status(NOT_FOUND)::build);
     }
 
     @DELETE
+    @Path("{id}")
     @Transactional
-    public void delete(Fruit fruit) {
-        Fruit f = Fruit.findById(fruit.id);
-        if (f != null) {
-            Fruit.deleteById(fruit.id);
-        } else {
-            throw new NotFoundException("Unknown fruit id : " + fruit.id);
-        }
+//    public void delete(Fruit fruit) {
+//        Fruit f = Fruit.findById(fruit.id);
+//        if (f != null) {
+//            Fruit.deleteById(fruit.id);
+//        } else {
+//            throw new NotFoundException("Unknown fruit id : " + fruit.id);
+//        }
+//    }
+    public Uni<Response> delete(@PathParam("id") Long id) {
+        return Fruit.deleteFruit(id)
+                .onItem().transform(entity -> !entity ? Response.serverError().status(NOT_FOUND).build()
+                        : Response.ok().status(OK).build());
     }
 }
